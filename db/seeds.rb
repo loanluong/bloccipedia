8c1d3f12a9b9103e793ad89c06b59ac5c063c69b
Lani Luong <loanlu@gmail.com>
2017-05-18 19:59:53 +0000

    Seeding data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Create Wikis
+50.times do
+    Wiki.create!(
+        title: Faker::Lorem.sentence,
+        body: Faker::Lorem.paragraph
+        )
+    end
+    wikis = Wiki.all
+
+10.times do
+    User.create!(
+        email: Faker::Internet.unique.email,
+        password: Faker::Lorem.characters(6)
+        )
+    end
+    users = User.all
+    
+    puts "Seed finished"
+    puts "#{Wiki.count} wikis created"
+    puts  "#{User.count} users created"
